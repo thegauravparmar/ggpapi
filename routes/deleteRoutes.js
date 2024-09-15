06175a7d10a6655f7688a7e1b16eef5f0b6f4da5
Nandini <Nandini Vaish>
2024-09-15 22:09:08 +0530

    update delete api route

--- routes/deleteRoutes.js
+++ routes/deleteRoutes.js
@@ -3,17 +3,17 @@ const Sale = require('../models/postModels'); // Import your Mongoose model
 const apiKeyMiddleware =require('./apikeymiddleware')
 const router = express.Router();
 
-router.delete("/delete/:height",apiKeyMiddleware, async (req, res) => {
-    let upheight = req.params.height;
+router.delete("/delete/:id",apiKeyMiddleware, async (req, res) => {
+    let id =req.params.id;
 
     try {
-        const sale = await Sale.deleteOne({ height: upheight });
+        const sale = await Sale.deleteOne({ _id: id });
         
         if (sale.modifiedCount > 0) {
             res.status(200).json({ object: sale, message: "Document successfully deleted" })
         }
         else {
-            res.status(200).json({ object: sale, message: "No document found with the given height" })
+            res.status(200).json({ object: sale, message: "No document found" })
         }
     }
     catch {
