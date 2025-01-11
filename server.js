f447e98eb7919f1d01a0b3f428d4a7a120f810bc
shakya94 <57297686+shakya94@users.noreply.github.com>
2025-01-12 03:08:19 +0530

    faq flyer add

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const userLoginApi = require("./sqlroutes/userloginapi"); // Import the userloginapi route file
 const logger = require("./sqlroutes/loggerController");
+const flyers = require("./sqlroutes/flyer");
+const faq = require("./sqlroutes/faq");
 const app = express();
 const port = 3000;
 
@@ -9,6 +11,8 @@ const port = 3000;
 app.use(bodyParser.json()); // Parse JSON bodies
 app.use("/api", userLoginApi); // Use the userLoginApi routes under "/api"
 app.use("/api", logger);
+app.use("/api", flyers);
+app.use("/api", faq);
 
 // Start server
 app.listen(port, () => {
---
+++ sqlroutes/faq.js
@@ -0,0 +1,60 @@
+const express = require("express"); // Import your Mongoose model
+const cors = require("../routes/cors");
+const db = require("../sqlconnection");
+const router = express.Router();
+const logger = require("../logger");
+
+
+  router.post("/faq", async (req, res) => {
+    try {
+      let { question, answer} = req.body;
+  
+      const query = "INSERT INTO faq (question, answer) VALUES (?, ?)";
+      
+      db.execute(query, [question, answer], (err, result) => {
+        if (err) {
+          console.error("Database error:", err);
+          return res.status(500).json({ error: "Database error" });
+        }
+        
+        res.status(201).json({
+          message: "Flyer created successfully",
+          faq: { id: result.insertId, question,answer}
+        });
+      });
+      
+    } catch (err) {
+      console.error("Server error:", err.message);
+      res.status(500).send("Server error");
+    }
+  });
+
+router.get("/faq", async (req, res) => {
+  const query = "SELECT * FROM faq";
+  db.execute(query, (err, results) => {
+    if (err) {
+      console.error("Error fetching flyers:", err);
+      return res.status(500).json({ error: "Database error" });
+    }
+    res.json(results);
+  });
+});
+
+router.delete('/faq/:id', async (req, res) => {
+    const faqId = req.params.id;
+
+    const query = "DELETE FROM faq WHERE id = ?";
+    db.execute(query,[faqId], (err, results) => {
+        if (err) {
+          console.error("Error deleting faq:", err);
+          return res.status(500).json({ error: "Database error" });
+        }
+        else if(results.affectedRows==0)
+            res.json({message:"ID not found"});
+        else
+        res.json({message:"Deleted Successfully"});
+      });
+});
+
+
+module.exports = router;
